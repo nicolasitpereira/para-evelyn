b8735f3e2d013c55b6422dba7aa3bf6d306bfb44
nicolasitpereira <nicolas.pereira.it@gmail.com>
2025-03-13 13:36:26 -0300

    Initial commit

---
+++ resultSim/script.js
@@ -0,0 +1,8 @@
+onload = () => {
+    const c = setTimeout(() => {
+      document.body.classList.remove("not-loaded");
+      clearTimeout(c);
+    }, 1000);
+  };
+
+  alert ('Evelyn, eu quero muito estar ao seu lado e, se você também sentir que podemos construir algo juntos, me comprometo a te fazer sentir amada e respeitada como você merece. Prometo estar presente, apoiar você e ser paciente com seu tempo, sempre respeitando seus sentimentos e desafios. Mas também preciso que você prometa, que vai se entregar ao processo, com a mesma intenção de criar algo genuíno e verdadeiro, para que, juntos, possamos construir um final feliz.')
